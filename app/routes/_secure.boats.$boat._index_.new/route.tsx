253ce7f0ff981f8143f74d143ede6676ad6b0195
James Bithell <Jbithell@users.noreply.github.com>
2024-08-25 09:03:37 +0100

    Add new boat route using RVF

--- app/routes/_secure.boats.$boat._index_.new/route.tsx
+++ app/routes/_secure.boats.$boat._index_.new/route.tsx
@@ -1,30 +1,31 @@
-import { Button, Input, Text } from "@mantine/core";
+import { Button, Text, TextInput } from "@mantine/core";
 import {
+  AppLoadContext,
   json,
   LoaderFunctionArgs,
   MetaFunction,
   redirect,
 } from "@remix-run/cloudflare";
 import type { ActionFunctionArgs } from "@remix-run/node";
-import { useActionData, useLoaderData } from "@remix-run/react";
-import {
-  isValidationErrorResponse,
-  useForm,
-  validationError,
-} from "@rvf/remix";
+import { Params, useActionData, useLoaderData } from "@remix-run/react";
+import { useForm, validationError } from "@rvf/remix";
 import { withZod } from "@rvf/zod";
 import { and, eq } from "drizzle-orm";
 import { z } from "zod";
 import { db } from "~/d1client.server";
 import { Boats } from "~/db/schema/Boats";
+import { LogEntries } from "~/db/schema/LogEntries";
 import { requireAuthenticatedUserId } from "~/utils/authsession.server";
 
 export const meta: MetaFunction = () => {
   return [{ title: "Boat" }];
 };
 
-export async function loader({ params, request, context }: LoaderFunctionArgs) {
-  const userId = await requireAuthenticatedUserId(request, context);
+const getBoatFromParam = async (
+  params: Params<string>,
+  context: AppLoadContext,
+  userId: number
+) => {
   if (
     typeof params.boat === "undefined" ||
     params.boat === null ||
@@ -34,9 +35,17 @@ export async function loader({ params, request, context }: LoaderFunctionArgs) {
   const boat = await db(context.cloudflare.env.DB).query.Boats.findFirst({
     columns: {
       name: true,
+      id: true,
     },
     where: and(eq(Boats.ownerId, userId), eq(Boats.uuid, params.boat)), // TODO abstract this into a function & grab the boats for the user too
   });
+  return boat;
+};
+
+export async function loader({ params, request, context }: LoaderFunctionArgs) {
+  const userId = await requireAuthenticatedUserId(request, context);
+  const boat = await getBoatFromParam(params, context, userId);
+  console.log(boat);
   if (typeof params.boat === "undefined" || boat === null || !boat)
     throw redirect("/boats");
   return json({
@@ -47,21 +56,33 @@ export async function loader({ params, request, context }: LoaderFunctionArgs) {
 
 const validator = withZod(
   z.object({
-    firstName: z.string().min(1, { message: "First name is required" }),
-    lastName: z.string().min(1, { message: "Last name is required" }),
-    email: z
-      .string()
-      .min(1, { message: "Email is required" })
-      .email("Must be a valid email"),
+    title: z.string().min(1, { message: "Title is required" }),
+    description: z.string().optional(),
+    latitude: z.number().gte(-90).lte(90).optional(),
+    longitude: z.number().gte(-180).lte(180).optional(),
   })
 );
 
-export const action = async ({ request }: ActionFunctionArgs) => {
+export const action = async ({
+  params,
+  request,
+  context,
+}: ActionFunctionArgs) => {
+  const userId = await requireAuthenticatedUserId(request, context);
+  const boat = await getBoatFromParam(params, context, userId);
+  if (typeof params.boat === "undefined" || boat === null || !boat)
+    throw redirect("/boats");
   const result = await validator.validate(await request.formData());
   if (result.error) return validationError(result.error, result.submittedData);
-  return {
-    message: `Submitted for ${result.data.firstName} ${result.data.lastName}!`,
-  };
+  await db(context.cloudflare.env.DB).insert(LogEntries).values({
+    boatId: boat.id,
+    title: result.data.title,
+    description: result.data.description,
+    latitude: result.data.latitude,
+    longitude: result.data.longitude,
+    source: "Web",
+  });
+  return null;
 };
 
 export default function App() {
@@ -70,39 +91,15 @@ export default function App() {
   const form = useForm({
     method: "post",
     validator,
-    defaultValues: {
-      firstName: "",
-      lastName: "",
-      email: "",
-    },
   });
 
   return (
     <form {...form.getFormProps()}>
+      {form.renderFormIdInput()}
       <Text>New entry for {loaderData.boat.name}</Text>
 
-      <Input
-        {...form.getControlProps("firstName")}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-          form.getControlProps("firstName").onChange(event.target.value)
-        }
-      />
-      <Input
-        {...form.getControlProps("lastName")}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-          form.getControlProps("lastName").onChange(event.target.value)
-        }
-      />
-      <Input
-        {...form.getControlProps("email")}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-          form.getControlProps("email").onChange(event.target.value)
-        }
-      />
+      <TextInput {...form.getControlProps("title")} label="Title" />
 
-      {errorData && !isValidationErrorResponse(errorData) && (
-        <Text>{errorData.message}</Text>
-      )}
       <Button type="submit" disabled={form.formState.isSubmitting}>
         {form.formState.isSubmitting ? "Creating..." : "Create"}
       </Button>
